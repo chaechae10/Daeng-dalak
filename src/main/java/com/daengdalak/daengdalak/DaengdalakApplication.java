e9e050c95d1d1535a4cd58e111e92f21dd0151de
김채연 <gimchaeyeon57@gmail.com>
2025-03-08 19:56:39 +0900

    initial commit

---
+++ src/main/java/com/daengdalak/daengdalak/DaengdalakApplication.java
@@ -0,0 +1,13 @@
+package com.daengdalak.daengdalak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaengdalakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaengdalakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daengdalak/daengdalak/DaengdalakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daengdalak.daengdalak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaengdalakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
